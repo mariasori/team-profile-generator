53e657987358324d36c8b30eb7db6b34a2117bf4
Maria Sori <mariasori.school@gmail.com>
2021-10-29 16:20:15 -0600

    inquirer questions for team members

---
+++ index.js
@@ -0,0 +1,134 @@
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+
+const teamArray = []
+
+const addTeamMember = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: `What role does this team member have?`,
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the team member's name?`,
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }   else {
+                    console.log('Please enter a name for the team member');
+                    return false;
+                }
+            }
+        },
+        {   
+            type: 'input',
+            name: 'id',
+            message: `What is the team member's ID number?`,
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                }   else {
+                    console.log('Please enter an ID number for the team member');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `What is the team member's e-mail address?`,
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                }   else {
+                    console.log('Please enter an e-mail address for the team member');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: `What is the manager's office number?`,
+            when: (input) => input.role === 'Manager',
+            validate: numberInput => {
+                if (numberInput) {
+                    return true
+                }   else {
+                    console.log(`Please enter the manager's office number`)
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `What is the engineer's github username?`,
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true
+                }   else {
+                    console.log(`Please enter the engineer's github username`)
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: `What school in the intern attending?`,
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true
+                }   else {
+                    console.log(`Please enter the intern's school`)
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
+        }
+    ])
+    .then(teamData => {
+        if (teamData.role === 'Manager') {
+            const { name, id, email, officeNumber } = teamData;
+            const manager = new Manager (name, id, email, officeNumber);
+
+            teamArray.push(manager);
+            console.log(manager);
+        } else if (teamData.role === 'Engineer') {
+            const { name, id, email, github } = teamData;
+            const engineer = new Engineer (name, id, email, github);
+
+            teamArray.push(engineer);
+            console.log(engineer);
+        } else {
+            const { name, id, email, school } = teamData;
+            const intern = new Intern (name, id, email, school)
+
+            teamArray.push(intern);
+            console.log(intern);
+        }
+        if (teamData.confirmAddEmployee) {
+            return addTeamMember(teamArray);
+        } else {
+            return teamArray;
+        }
+    })
+}
+
+addTeamMember()
